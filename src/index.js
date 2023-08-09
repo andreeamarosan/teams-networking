4fab4910af1f79b60f3dce2ad29f904927fc0b6d
Andreea Marosan <andreeamarosan80@gmail.com>
2023-08-09 15:33:54 +0300

    forse render after cancel edit (-1 as editId)

--- src/index.js
+++ src/index.js
@@ -174,7 +174,7 @@ function initEvents() {
     if (editId) {
       // console.warn("cancel edit");
       allTeams = [...allTeams];
-      renderTeams(allTeams);
+      renderTeams(allTeams, -1); // use -1 to force render
       setInputsDisabled(false);
       editId = "";
     }
