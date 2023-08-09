91effa9f1448508ba16d143728132e2614882013
Andreea Marosan <andreeamarosan80@gmail.com>
2023-08-09 15:25:20 +0300

    improve mask/unmask using el as selector (string)

--- src/index.js
+++ src/index.js
@@ -70,12 +70,12 @@ function addTitlesToOverflowCells() {
 }
 
 async function loadTeams() {
-  mask($("#teamsForm"));
+  mask("#teamsForm");
   const teams = await loadTeamsRequest();
   console.warn("teams", teams);
   allTeams = teams;
   renderTeams(teams);
-  unmask($("#teamsForm"));
+  unmask("#teamsForm");
 }
 function getTeamValues(parent) {
   const promotion = $(`${parent} input[name=promotion]`).value;
@@ -181,7 +181,7 @@ function initEvents() {
     if (e.target.matches("button.delete-btn")) {
       const id = e.target.dataset.id;
       //console.warn("delete... %o", id);
-      mask($("#teamsForm"));
+      mask("#teamsForm");
       deleteTeamRequest(id, status => {
         console.info("delete callback %o", status);
         if (status.success) {
